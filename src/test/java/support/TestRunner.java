255709bc7458edecf9488d3c46ab39d2b2bff13d
Max Smuli <shmax25@gmail.com>
2020-03-07 17:14:45 -0800

    Added TestRunner class, from this class could be executed tests

---
+++ src/test/java/support/TestRunner.java
@@ -0,0 +1,33 @@
+package support;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.TimeUnit;
+
+import static support.TestContext.getDriver;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber/report.json"},
+        features = "src/test/resources/features",
+        glue = {"definitions", "support"}
+)
+
+public class TestRunner {
+  @BeforeClass
+  public static void setup() {
+    System.out.println("BeforeAll");
+    getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    getDriver().manage().window().maximize();
+  }
+
+  @AfterClass
+  public static void teardown() {
+    System.out.println("AfterAll");
+    getDriver().quit();
+  }
+}
